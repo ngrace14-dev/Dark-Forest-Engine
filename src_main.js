5a7871fad180c5465071e2e30adfb981a31d48d7
ngrace14-dev <ngrace@obsidiancitadelprotocol.com>
2026-09-15 18:52:07 -0700

    .

--- src_main.js
+++ src_main.js
@@ -1,25 +1,24 @@
 // File: src_main.js
+// 1. Core Data
 import './src_core_state.js';
+// 2. Systems (Must load before Engine boot)
 import './src_systems_inventory.js';
 import './src_systems_ui.js';
 import './src_systems_input.js';
 import './src_systems_audio.js';
-import './src_systems_ai.js';
-import './src_systems_dev_tools.js';
 import './src_systems_vfx.js';
-import './src_systems_world.js';
-import './src_systems_assets.js';
 import './src_systems_navigation.js';
+import './src_systems_roads.js';
 import './src_systems_villages.js';
 import './src_systems_adventurers.js';
-import './src_systems_roads.js';
+import './src_systems_ai.js';
+import './src_systems_assets.js';
+// 3. Mod Tools / Overlays
 import './src_firebase.js';
 import './src_systems_editor.js';
+import './src_systems_dev_tools.js';
 import './src_systems_health.js';
 
-
-
-
-
-
+// 4. Main Loop & Boot
 import './src_engine.js';
+
