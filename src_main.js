c9cf4496ec8c929995e98e5b0ac65e66ca782832
ngrace14-dev <ngrace@obsidiancitadelprotocol.com>
2026-09-20 23:18:57 -0700

    Update src_main.js

--- src_main.js
+++ src_main.js
@@ -18,6 +18,7 @@ import './src_systems_block_terrain.js';     // NEW: Procedural Block Terrain & 
 import './src_systems_grass.js';
 import './src_systems_ruins.js';            // <-- ADDED: Procedural Stone Ruins & Cobblestones
 import './src_systems_procedural_trees.js';  // <-- ADDED: High-Quality Procedural Trees
+import './src_systems_forest_impostors.js';   // <-- ADDED: 3km Distant Canopy Impostor System
 import './src_systems_input.js';
 import './src_systems_audio.js';
 import './src_systems_vfx.js';
